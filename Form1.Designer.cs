848da5ca40ffbcef09181c13b5da9e14356d5933
Sagar Shah <shahsagu787@gmail.com>
2022-01-23 13:54:00 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.GreenYellow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Broadway", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 40);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tic-Tac-Toe";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox7.Location = new System.Drawing.Point(238, 327);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox7.TabIndex = 18;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox8.Location = new System.Drawing.Point(351, 327);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox8.TabIndex = 17;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox9.Location = new System.Drawing.Point(466, 327);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox9.TabIndex = 16;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.Location = new System.Drawing.Point(238, 212);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.Location = new System.Drawing.Point(351, 212);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox6.Location = new System.Drawing.Point(466, 212);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox6.TabIndex = 13;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(466, 94);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(351, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(238, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToe.Properties.Resources.Intermediate_Background;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
+*/
+
+
+/*
+ * Project : TicTacToe
+ * Revesion History : Created by Sagar Shah on September 2021
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        //Declairing and Initializing Global Variables
+        Image[] imageName = new Image[]
+        {
+            Properties.Resources.X, Properties.Resources.O
+        };
+        int counter = 1;
+        bool flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Init()
+        {
+            foreach (Control item in Controls)
+            {
+                
+                if (item is PictureBox)
+                {
+                    
+                    PictureBox tile = (PictureBox)item;
+                    tile.SizeMode = PictureBoxSizeMode.StretchImage;
+                    tile.Click += tile_Click;
+
+                }
+
+            }
+
+        }
+
+
+        public void tile_Click(object sender, EventArgs e)
+        {
+           
+            PictureBox tile = (PictureBox)sender;
+            if (tile.Image == null)
+            {
+                //Checking Condition for assigning the image to tile
+                if (counter % 2 == 0)
+                {
+                    tile.Image = imageName[1];
+                    tile.Tag = "O";
+
+                }
+                else
+                {
+                    tile.Image = imageName[0];
+                    tile.Tag = "X";
+
+                }
+                //Checking For Horizontal Wins
+                #region Horizontal Wins
+                if ((pictureBox1.Image != null) && (pictureBox2.Image != null) && (pictureBox3.Image != null))
+                {
+                    if ((pictureBox1.Tag == pictureBox2.Tag) && (pictureBox2.Tag == pictureBox3.Tag))
+                    {
+                        MessageBox.Show(pictureBox1.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+
+                if ((pictureBox4.Image != null) && (pictureBox5.Image != null) && (pictureBox6.Image != null))
+                {
+                    if ((pictureBox4.Tag == pictureBox5.Tag) && (pictureBox5.Tag == pictureBox6.Tag))
+                    {
+                        MessageBox.Show(pictureBox4.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+
+                if ((pictureBox7.Image != null) && (pictureBox8.Image != null) && (pictureBox9.Image != null))
+                {
+                    if ((pictureBox7.Tag == pictureBox8.Tag) && (pictureBox8.Tag == pictureBox9.Tag))
+                    {
+                        MessageBox.Show(pictureBox7.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+                #endregion
+
+                //Checking For Vertical Wins
+                #region Vertical Wins
+                if ((pictureBox1.Image != null) && (pictureBox4.Image != null) && (pictureBox7.Image != null))
+                {
+                    if ((pictureBox1.Tag == pictureBox4.Tag) && (pictureBox4.Tag == pictureBox7.Tag))
+                    {
+                        MessageBox.Show(pictureBox1.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+
+                if ((pictureBox2.Image != null) && (pictureBox5.Image != null) && (pictureBox8.Image != null))
+                {
+                    if ((pictureBox2.Tag == pictureBox5.Tag) && (pictureBox5.Tag == pictureBox8.Tag))
+                    {
+                        MessageBox.Show(pictureBox2.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+
+                if ((pictureBox3.Image != null) && (pictureBox6.Image != null) && (pictureBox9.Image != null))
+                {
+                    if ((pictureBox3.Tag == pictureBox6.Tag) && (pictureBox6.Tag == pictureBox9.Tag))
+                    {
+                        MessageBox.Show(pictureBox3.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+                #endregion
+
+                //Checking For Diagonal Wins
+                #region Diagonal Wins
+                if ((pictureBox1.Image != null) && (pictureBox5.Image != null) && (pictureBox9.Image != null))
+                {
+                    if ((pictureBox1.Tag == pictureBox5.Tag) && (pictureBox5.Tag == pictureBox9.Tag))
+                    {
+                        MessageBox.Show(pictureBox1.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+
+                if ((pictureBox3.Image != null) && (pictureBox5.Image != null) && (pictureBox7.Image != null))
+                {
+                    if ((pictureBox3.Tag == pictureBox5.Tag) && (pictureBox5.Tag == pictureBox7.Tag))
+                    {
+                        MessageBox.Show(pictureBox3.Tag + " Wins", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        flag = true;
+                    }
+                }
+                #endregion
+
+                //Checking For Draw
+                #region Draw
+                if (counter >= 9 && !flag)
+                {
+                    MessageBox.Show("Draw", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    flag = true;
+                }
+                #endregion
+                counter++;
+
+                //Checking flag to reset the game
+                if (flag)
+                {
+                    #region Reset
+                    //Reset PictureBox
+                    pictureBox1.Image = null;
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                    pictureBox4.Image = null;
+                    pictureBox5.Image = null;
+                    pictureBox6.Image = null;
+                    pictureBox7.Image = null;
+                    pictureBox8.Image = null;
+                    pictureBox9.Image = null;
+
+                    //Reset PictureBox Tags
+                    pictureBox1.Tag = null;
+                    pictureBox2.Tag = null;
+                    pictureBox3.Tag = null;
+                    pictureBox4.Tag = null;
+                    pictureBox5.Tag = null;
+                    pictureBox6.Tag = null;
+                    pictureBox7.Tag = null;
+                    pictureBox8.Tag = null;
+                    pictureBox9.Tag = null;
+
+                    //Reset counter to 1 and flag as false
+                    counter = 1;
+                    flag = false;
+                    #endregion
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            
+            Init();
+        }
+    }
+}
